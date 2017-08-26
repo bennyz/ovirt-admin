9e409c327b9d0ac58b2ff3514cef2f5838baeeb4
Benny Zlotnik <bzlotnik@redhat.com>
2017-08-26 15:15:13 +0300

    Add header

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <b-navbar toggleable="md" type="dark" variant="info">
+            <b-nav-toggle target="nav_collapse"></b-nav-toggle>
+            <b-navbar-brand href="#">oVirt</b-navbar-brand>
+            <b-collapse is-nav id="nav_collapse">
+                <b-nav is-nav-bar>
+                <b-nav-item href="#">Datacenters</b-nav-item>
+                </b-nav>
+            </b-collapse>
+        </b-navbar>
+    </div>
+</template>
+<script>
+
+</script>
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
-import Element from 'element-ui'
+import BootstrapVue from 'bootstrap-vue'
 
 import { routes } from './routes';
 import store from './store/store';
 
 Vue.use(VueRouter);
-Vue.use(Element);
+Vue.use(BootstrapVue);
 
 const router = new VueRouter({
     mode: 'history',
